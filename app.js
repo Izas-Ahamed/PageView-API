0b6a70352cecb2a395ccb211be55d98da82f78d1
Izas-Ahamed <60055462+Izas-Ahamed@users.noreply.github.com>
2021-04-03 22:34:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,16 @@ var server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Methods", "OPTIONS, DELETE");
     res.end("View Deleted Successfully 🗑️");
   }
+
+  if (req.url === "/addclick" && req.method === "POST") {
+    writeClicksCount();
+    res.setHeader("Content-Type", "application/text");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Request-Method", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST");
+    res.end("Page Click Added Successfully 😉");
+  }
 });
 
 var readFile = () => {
@@ -46,7 +56,19 @@ var writeFile = () => {
   if (data.pageViewed) {
     data.pageViewed = data.pageViewed + 1;
   } else {
-    data = { pageViewed: 1 };
+    data = { pageViewed: 1, clicks: data.clicks };
+  }
+  fs.writeFileSync("views.json", JSON.stringify(data), (err) => {
+    console.log(err);
+  });
+};
+
+var writeClicksCount = () => {
+  var data = JSON.parse(readFile());
+  if (data.clicks >= 0) {
+    data.clicks = data.clicks + 1;
+  } else {
+    data = { pageViewed: data.pageViewed, clicks: 1 };
   }
   fs.writeFileSync("views.json", JSON.stringify(data), (err) => {
     console.log(err);
@@ -54,10 +76,11 @@ var writeFile = () => {
 };
 
 var resetFile = () => {
-  data = { pageViewed: 0 };
-  fs.writeFileSync("views.json", "{}", (err) => {
+  data = { pageViewed: 0, clicks: 0 };
+  fs.writeFileSync("views.json", data, (err) => {
     console.log(err);
   });
 };
+
 const port = process.env.PORT || 3000;
 server.listen(port);
